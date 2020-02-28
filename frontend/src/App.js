5e6f9034db2363289b7b4e08cee8df06f92621f6
jxjshaw <jxjshaw@gmail.com>
2020-02-27 18:04:08 -0800

    new frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main.js';
+
+function App() {
+  return (
+    <div className="App">
+       <div>
+          <h1>File Upload</h1>
+           <Main />
+       </div>
+    </div>
+  );
+}
+
+export default App;
